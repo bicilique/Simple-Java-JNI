02b6316c307f020f8ecffd4cdcfaf61bb685ec4a
Afif Faizianur <afiffaizianur@gmail.com>
2024-01-25 23:01:35 +0700

    Add : 1. Simple ExampleParameters

--- src/main/java/org/example/HelloWorldJNI.java
+++ src/main/java/org/example/HelloWorldJNI.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class HelloWorldJNI {
     static {
-        System.load(HelloWorldJNI.class.getResource("/SampleHelloWorld/native.dll").getPath());
+        System.load(HelloWorldJNI.class.getResource("/SampleParameters/native.dll").getPath());
     }
     public static void main(String[] args) {
         new HelloWorldJNI().sayHello();
